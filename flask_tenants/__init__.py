ecdeebbc27877941431972d985c96d8be032e845
Cory Cline <cory@corycline.com>
2024-07-05 18:50:26 -0500

    Restructured and abstracted the module such that it can be implemented in a cleaner way.

--- flask_tenants/__init__.py
+++ flask_tenants/__init__.py
@@ -1,36 +1,14 @@
-from .middleware import MultiTenancyMiddleware, create_tenancy
+from .middleware import MultiTenancyMiddleware, create_tenancy, FlaskTenants
 from .models import BaseTenant, BaseDomain, db
 from .utils import register_event_listeners, register_engine_event_listeners
-from .exceptions import (TenantActivationError,
-                         TenantNotFoundError,
-                         SchemaRenameError,
-                         SchemaCreationError,
-                         SchemaDropError,
-                         TableCreationError
-                         )
+from .exceptions import (TenantActivationError, TenantNotFoundError, SchemaRenameError, SchemaCreationError,
+                         SchemaDropError, TableCreationError)
 
 
 def init_app(app, tenant_model=None, domain_model=None):
-    db.init_app(app)
-
-    if tenant_model:
-        setattr(db.Model, 'Tenant', tenant_model)
-    if domain_model:
-        setattr(db.Model, 'Domain', domain_model)
-
-    with app.app_context():
-        register_event_listeners()
-        register_engine_event_listeners(db.engine)
+    flask_tenants = FlaskTenants(app, tenant_model, domain_model, db)
+    flask_tenants.init()
 
 
-__all__ = ['init_app',
-           'BaseTenant',
-           'BaseDomain',
-           'create_tenancy',
-           'TenantActivationError',
-           'TenantNotFoundError',
-           'SchemaRenameError',
-           'SchemaCreationError',
-           'SchemaDropError',
-           'TableCreationError'
-           ]
+__all__ = ['init_app', 'BaseTenant', 'BaseDomain', 'create_tenancy', 'FlaskTenants', 'TenantActivationError',
+           'TenantNotFoundError', 'SchemaRenameError', 'SchemaCreationError', 'SchemaDropError', 'TableCreationError']
